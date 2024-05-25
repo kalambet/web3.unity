45a033ea005cc5e82167db8b77401388bed89831
MakMuftic <mak@chainsafe.io>
2024-05-25 18:47:24 +0200

    update session manager

---
+++ src/ChainSafe.Gaming.AltLayer/Dto/SessionJoined.cs
@@ -0,0 +1,21 @@
+namespace ChainSafe.Gaming.AltLayer.Contracts
+{
+    using System.Numerics;
+    using Nethereum.ABI.FunctionEncoding.Attributes;
+
+    [Event("SessionJoined")]
+    public class SessionJoinedEventDTO : IEventDTO
+    {
+        [Parameter("uint256", "id", 1, false)]
+        public BigInteger Id { get; set; }
+
+        [Parameter("address", "miner", 2, true)]
+        public string Miner { get; set; }
+
+        [Parameter("address", "defender", 3, true)]
+        public string Defender { get; set; }
+
+        [Parameter("string", "rpcUrl", 4, false)]
+        public string RpcUrl { get; set; }
+    }
+}
